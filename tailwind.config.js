4b22fc337ef483d33b438c108eaca5937d449d2c
SnugElf <60815736+SnugElf@users.noreply.github.com>
2023-01-19 02:04:42 +0200

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'cardsscreen': '1145px',
+    }
+  },
+  plugins: [],
+}
